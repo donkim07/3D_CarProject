b85f13e89881bce3f3d6a7a9e22979f6842109bf
Donald <donestomih@gmail.com>
2025-05-13 23:57:40 +0300

    refactor: Replace Hive initialization with DatabaseService and ModelCacheService setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_3d/startup.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_3d/services/database_service.dart';
+import 'package:flutter_3d/services/model_cache_service.dart';
 
 void main() async {
-  await Hive.initFlutter();
-  await Hive.openBox('student');
-  await Hive.initFlutter();
-  await Hive.openBox('student-email');
-  await Hive.initFlutter();
-  await Hive.openBox('student-password');
-  await Hive.initFlutter();
-  await Hive.openBox('lecturer');
-  await Hive.initFlutter();
-  await Hive.openBox('lecturer-email');
-  await Hive.initFlutter();
-  await Hive.openBox('lecturer-password');
-  await Hive.initFlutter();
-  await Hive.openBox('lessons');
-  await Hive.initFlutter();
-  await Hive.openBox('showlesson');
-  await Hive.initFlutter();
-  await Hive.openBox('showlesson2');
-  await Hive.initFlutter();
-  await Hive.openBox('feedback');
-  await Hive.initFlutter();
-  await Hive.openBox('storedstudent');
-  await Hive.initFlutter();
-  await Hive.openBox('namedfeedback');
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize database
+  await DatabaseService.instance.initialize();
+  
+  // Initialize model cache service
+  final modelCache = ModelCacheService();
 
   runApp(MyApp());
 }
